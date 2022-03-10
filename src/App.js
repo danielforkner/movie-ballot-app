5e471edc7c325167b9d9c3ba0918c691b46458d0
Moriah King <moriahk.@Moriahs-MacBook-Air.local>
2022-03-10 18:13:10 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { PollsContainer, AddPollForm } from './components';
-import './index.css';
+import React, { useState } from "react";
+import { PollsContainer, AddPollForm, SinglePoll } from "./components";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./index.css";
 
 const dummyData = {
   info: { length: 3 },
   data: [
-    { id: 1, name: 'movie night', pollType: 'single' },
-    { id: 2, name: 'round robin', pollType: 'double' },
-    { id: 3, name: 'movie', pollType: 'triple' },
+    { id: 1, name: "movie night", pollType: "single" },
+    { id: 2, name: "round robin", pollType: "double" },
+    { id: 3, name: "movie", pollType: "triple" },
   ],
 };
 
@@ -15,10 +16,28 @@ const App = () => {
   const [polls, setPolls] = useState(dummyData);
 
   return (
-    <div className="pollsContainer">
-      <AddPollForm setPolls={setPolls} polls={polls} />
-      <PollsContainer setPolls={setPolls} polls={polls} />
-    </div>
+    <Router>
+      <Link to="/">Home</Link>
+      <Switch>
+        <Route path="/">
+          <AddPollForm />
+        </Route>
+      </Switch>
+    </Router>
+    // <Router>
+    //   <Link to="/">Home</Link>
+    //   <Routes>
+    //     {/* <Route path="/singlePoll">
+    //     <SinglePoll />
+    //   </Route> */}
+    //     <Route path="/">
+    //       <div className="allPollsContainer">
+    //         <AddPollForm setPolls={setPolls} polls={polls} />
+    //         <PollsContainer setPolls={setPolls} polls={polls} />
+    //       </div>
+    //     </Route>
+    //   </Routes>
+    // </Router>
   );
 };
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as AddPollForm } from './AddPollForm';
 export { default as PollsContainer } from './PollsContainer';
+export { default as SinglePoll} from './SinglePoll'
