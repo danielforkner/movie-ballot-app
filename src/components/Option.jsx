45e48adef132c61399041ff85cb104e3fac8fdcc
danielforkner <daniel.forkner@gmail.com>
2022-03-13 14:57:25 -0400

    added non-functional generate link btn

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { fetchMovies } from '../api/fetch';
 import ChosenOptions from './ChosenOptions';
 
@@ -8,77 +8,82 @@ const Option = ({ setPolls, polls, currentPoll, pollID }) => {
   const [searchResults, setSearchResults] = useState([]);
 
   return (
-    <div className="optionContainer">
-      <h4>{currentPoll.options.option1.name} (rename)</h4>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          // isSearchingTRUE display spinning gear
-          try {
-            const data = await fetchMovies(title, year);
-            console.log(data);
-            if (data.Response === 'False') throw new Error(`${data.Error}`);
-            setSearchResults(data.Search);
-          } catch (error) {
-            console.error(error);
-          } finally {
-            // isSearchingFALSE
-            setTitle('');
-            setYear('');
-          }
-        }}
-      >
-        <label htmlFor="title">Title:</label>
-        <input
-          required
-          type="text"
-          value={title}
-          name="title"
-          placeholder="Home Alone"
-          onChange={(e) => {
-            setTitle(e.target.value);
+    <Fragment>
+      <div className="optionContainer">
+        <h4>{currentPoll.options.option1.name} (rename)</h4>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            // isSearchingTRUE display spinning gear
+            try {
+              const data = await fetchMovies(title, year);
+              console.log(data);
+              if (data.Response === 'False') throw new Error(`${data.Error}`);
+              setSearchResults(data.Search);
+            } catch (error) {
+              console.error(error);
+            } finally {
+              // isSearchingFALSE
+              setTitle('');
+              setYear('');
+            }
           }}
-        />
-        <label htmlFor="year">Year:</label>
-        <input
-          type="text"
-          value={year}
-          name="year"
-          placeholder="1990"
-          onChange={(e) => {
-            setYear(e.target.value);
-          }}
-        />
-        <button type="submit">Search for Movies</button>
-      </form>
-      <div className="selectOptionForm">
-        <span>
-          <em>Click to add</em>
-        </span>
-        {searchResults.map((movie, i) => {
-          return (
-            <div
-              className="option"
-              key={i}
-              onClick={() => {
-                currentPoll.options.option1.movies.push(movie);
-                setPolls({
-                  info: polls.info,
-                  data: [
-                    ...polls.data.filter((element) => +pollID !== element.id),
-                    currentPoll,
-                  ],
-                });
-                console.log('new polls.data', polls.data);
-              }}
-            >
-              {movie.Title}, ({movie.Year})
-            </div>
-          );
-        }, [])}
+        >
+          <label htmlFor="title">Title:</label>
+          <input
+            required
+            type="text"
+            value={title}
+            name="title"
+            placeholder="Home Alone"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <label htmlFor="year">Year:</label>
+          <input
+            type="text"
+            value={year}
+            name="year"
+            placeholder="1990"
+            onChange={(e) => {
+              setYear(e.target.value);
+            }}
+          />
+          <button type="submit">Search for Movies</button>
+        </form>
+        <div className="selectOptionForm">
+          <span>
+            <em>Click to add</em>
+          </span>
+          {searchResults.map((movie, i) => {
+            return (
+              <div
+                className="option"
+                key={i}
+                onClick={() => {
+                  currentPoll.options.option1.movies.push(movie);
+                  setPolls({
+                    info: polls.info,
+                    data: [
+                      ...polls.data.filter((element) => +pollID !== element.id),
+                      currentPoll,
+                    ],
+                  });
+                  console.log('new polls.data', polls.data);
+                }}
+              >
+                {movie.Title}, ({movie.Year})
+              </div>
+            );
+          }, [])}
+        </div>
+        <ChosenOptions chosen={currentPoll.options.option1.movies} />
       </div>
-      <ChosenOptions chosen={currentPoll.options.option1.movies} />
-    </div>
+      <button className="generateLinkBtn">
+        FINALIZE POLL AND GENERATE LINK
+      </button>
+    </Fragment>
   );
 };
 
