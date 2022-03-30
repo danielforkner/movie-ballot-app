86af4983b75000d9adaf427ffec0edd185ccb85e
danielforkner <daniel.forkner@gmail.com>
2022-03-29 21:39:08 -0400

    sroutes that don't match server routes will be handled by react router

--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+
 const express = require('express');
-const server = express();
+const server = express(); // create express app
 const apiRouter = require('./api');
 const morgan = require('morgan');
 
 server.use(morgan('dev'));
 
+// LOAD REACT APP
 server.use(express.static(path.join(__dirname, '..', 'build')));
 server.use(express.static('public'));
-server.get('/', (req, res, next) => {
+
+// SERVER ROUTES
+server.use('/api', apiRouter);
+
+// IF NONE OF THE INCOMING ROUTES MATCH A SERVER ROUTE,
+// THE SERVER WILL TRANSFER THE ROUTE TO REACT ROUTER
+server.use((req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-server.use('/api', apiRouter);
-
+// START THE SERVER
 server.listen(PORT, () => {
   console.log('Server is LIVE on port', PORT);
 });
