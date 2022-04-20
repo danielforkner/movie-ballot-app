8b09314846b0d29f72880119928228a8b51abc66
danielforkner <daniel.forkner@gmail.com>
2022-04-20 19:42:33 -0400

    heroku

--- db/client.js
+++ db/client.js
@@ -2,8 +2,13 @@ const { Client } = require('pg');
 
 const DB_NAME = 'movie-app';
 
-const DB_URL =
-  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
-const client = new Client(DB_URL);
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || `postgres://localhost:5432/movie-app`,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
 
 module.exports = client;
