d8ee4589295b6638de2145694f05c2bfd00c3ce9
danielforkner <daniel.forkner@gmail.com>
2022-04-20 19:10:16 -0400

    save

--- client/src/components/NAVBAR/Navbar.jsx
+++ client/src/components/NAVBAR/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Login, Register } from './';
+import { Login, Register } from '.';
 import useAuth from '../hooks/useAuth';
 
 const Navbar = () => {
